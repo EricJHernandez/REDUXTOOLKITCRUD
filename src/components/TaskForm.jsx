ac115fe18898056ad49b5fbea5963f55b8529c64
Eric Hernandez <ejhernandez@apexsystems.com>
2023-03-22 22:49:00 -0600

    Completed Redux

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,44 +1,74 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { addTask } from '../features/tasks/taskSlice';
-import { v4 as uuid } from 'uuid';
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { addTask, editTask } from "../features/tasks/tasksSlice";
+import { v4 as uuid } from "uuid";
+
 
 export const TaskForm = () => {
-
     const [task, setTask] = useState({
-        title: '',
-        description: ''
-    })
-
+        title: "",
+        description: "",
+    });
     const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const params = useParams();
+    const tasks = useSelector((state) => state.tasks);
 
-    const handleChange = e => {
+    const handleChange = (e) => {
         setTask({
             ...task,
-            [e.target.name]: e.target.value
-        })
-    }
+            [e.target.name]: e.target.value,
+        });
+    };
+
     const handleSubmit = (e) => {
-        e.preventDefault()
-        dispatch(addTask(task))
-    }
+        e.preventDefault();
+
+        if (params.id) {
+            dispatch(editTask({ ...task, id: params.id }));
+        } else {
+            dispatch(
+                addTask({
+                    ...task,
+                    id: uuid(),
+                })
+            );
+        }
+
+        navigate("/");
+    };
+
+    useEffect(() => {
+        if (params.id) {
+            setTask(tasks.find((task) => task.id === params.id));
+        }
+    }, [params, tasks]);
 
     return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    name='title'
-                    placeholder='Title'
-                    onChange={handleChange}
-                />
+        <form onSubmit={handleSubmit} className="bg-zinc-800 max-w-sm p-4">
+            <label className="block text-sm font-bold">Task:</label>
+            <input
+                type="text"
+                name="title"
+                onChange={handleChange}
+                value={task.title}
+                className="w-full p-2 rounded-md bg-zinc-600 mb-2"
+                placeholder="Write a title"
+                autoFocus
+            />
+            <label>
+                Description:
                 <textarea
+                    type="text"
                     name="description"
-                    placeholder='Description'
                     onChange={handleChange}
-                ></textarea>
-                <button>Save</button>
-            </form>
-        </>
-    )
+                    value={task.description}
+                    className="w-full p-2 rounded-md bg-zinc-600 mb-2"
+                    placeholder="Write a description"
+                />
+            </label>
+            <button type="submit" className="bg-indigo-600 px-2 py-1">Submit</button>
+        </form>
+    );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { App } from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 
+import './index.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<Provider store={store}>
